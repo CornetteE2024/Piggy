a12def41e6f8f26cc3bd76d64519e4895428bbb5
Elizabeth Cornette <91563177+CornetteE2024@users.noreply.github.com>
2021-11-04 18:51:33 +0000

    Experiment 4

--- student.py
+++ student.py
@@ -62,6 +62,7 @@ class Piggy(PiggyParent):
     def skri(self):
       self.deg_fwd(360)
       time.sleep(2)
+      self.stop()
       self.right(primary = 40, counter = -40)
       time.sleep(1)
       self.stop()
