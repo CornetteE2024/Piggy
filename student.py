3c95bcf90ca09b51dc869690f9a9d2a86cf403d4
Elizabeth Cornette <91563177+CornetteE2024@users.noreply.github.com>
2021-11-04 18:23:22 +0000

    sleeping

--- student.py
+++ student.py
@@ -59,6 +59,7 @@ class Piggy(PiggyParent):
     '''
     def skri(self):
       self.deg_fwd(360)
+      time.sleep(3)
       self.right(primary = 40, counter = -40)
       time.sleep(1)
       self.stop()
