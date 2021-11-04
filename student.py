98486cb005c871551215287b34de7c1486fe09c8
Elizabeth Cornette <91563177+CornetteE2024@users.noreply.github.com>
2021-11-04 19:10:28 +0000

    version 23

--- student.py
+++ student.py
@@ -60,7 +60,7 @@ class Piggy(PiggyParent):
 
     #Square = Skri Test
     def skri(self):
-      self.fwd(360)
+      self.fwd()
       time.sleep(2)
       self.stop()
       self.right(primary = 40, counter = -40)
