aee44e003c208b3c1c2abef9387a49dd3f04e2cb
Elizabeth Cornette <91563177+CornetteE2024@users.noreply.github.com>
2021-11-15 19:52:52 +0000

    version 42

--- student.py
+++ student.py
@@ -92,9 +92,9 @@ class Piggy(PiggyParent):
     def vanek(self):
       while True:
         self.read_distance()
-        if self.read_distance <= 20:
+        if self.read_distance < 20:
           self.stop()
-        elif self.read_distance >= 21:
+        elif self.read_distance > 20:
           self.fwd()
         
     
