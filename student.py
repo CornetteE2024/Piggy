2137775ab606964a6e51ab500710626a416829d6
Elizabeth Cornette <91563177+CornetteE2024@users.noreply.github.com>
2021-11-23 19:48:14 +0000

    version 60

--- student.py
+++ student.py
@@ -73,7 +73,7 @@ class Piggy(PiggyParent):
           time.sleep(.75)
           self.stop()
           self.left(primary = -40, counter = 40)
-          time.sleep(.75)
+          time.sleep(2)
           self.fwd()
           #self.servo(1750)
           #self.read_distance()
